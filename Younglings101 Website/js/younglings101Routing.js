0535ed1681f8d4342115eef7ee10d6054693c9fe
Dewald.Mouton <moutondewald@gmail.com>
2017-08-09 21:53:18 +0200

    Fixes
    Fixed routing and Font Awesome

--- Younglings101 Website/js/younglings101Routing.js
+++ Younglings101 Website/js/younglings101Routing.js
@@ -2,19 +2,19 @@ var app = angular.module('main', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider){
     $routeProvider
     .when('/', {
-        templateUrl: 'C:\Users\Ernst\Documents\GitHub\Younglings101Webpage\Younglings101 Website\socialMedia\twitter.html'
+        templateUrl: "socialMedia/twitter.html"
     })
     .when('/facebook', {
-        templateUrl: 'C:\Users\Ernst\Documents\GitHub\Younglings101Webpage\Younglings101 Website\socialMedia\facebook.html'
+        templateUrl: "socialMedia/facebook.html"
     })
     .when('/instagram', {
-        templateUrl: 'C:\Users\Ernst\Documents\GitHub\Younglings101Webpage\Younglings101 Website\socialMedia\instagram.html'
+        templateUrl: "socialMedia/instagram.html"
     })
     .when('/youtube', {
-        templateUrl: 'C:\Users\Ernst\Documents\GitHub\Younglings101Webpage\Younglings101 Website\socialMedia\youtube.html'
+        templateUrl: "socialMedia/youtube.html"
     })
     .when('/linkedin', {
-        templateUrl: 'C:\Users\Ernst\Documents\GitHub\Younglings101Webpage\Younglings101 Website\socialMedia\linkedin.html'
+        templateUrl: "socialMedia/linkedin.html"
     })
     .otherwise({
         redirectTo: '/'
