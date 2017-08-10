92f7328e52ff8cd84404516b96f4f8fa281957a6
E-Kaese <ernstkaese@gmail.com>
2017-08-10 23:02:03 +0200

    Thu Aug 10 23:01:54 CAT 2017

--- Younglings101 Website/js/currentYounglingRouting.js
+++ Younglings101 Website/js/currentYounglingRouting.js
@@ -1,10 +1,24 @@
-var app = angular.module("main", ['ngRoute']);
-
-app.config(['$routeProvider', function($routeProvider) {
+var app = angular.module("main", ["ngRoute"]);
+app.config(function ($routeProvider) {
     $routeProvider
-        .when('/', {
-            templateUrl: "pages/younglingsProfiles.html"
-        }).otherwise({
-            redirectTo: '/'
-        });
-}]);
+    .when("/ernst", {
+        templateUrl: "../pages/younglingsProfilesSites/ernst/index.html"
+    })
+    .when("/malcolm", {
+        templateUrl: "../pages/younglingsProfilesSites/malcolm/malcolmyoungling.html"
+    })
+    .otherwise({
+        template: ""
+    });
+})
+.directive('card', function() {
+    return {
+        restrict: 'E',
+        template: "<li class='col-xs-4'><figure class='younglings'><img src='http://via.placeholder.com/350x500' alt='a picture' class='img-responsive'/><figcaption><h3>Name <span>Surname</span></h3></figcaption></figure></li>"
+    };
+})
+.controller('randomiser', function(){
+    var links = [["#!ernst","../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"], ["#!malcolm","../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"]];
+    return links[Math.random*links.length];
+});
+
