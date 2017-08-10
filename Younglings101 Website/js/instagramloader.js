7347da6539a85546fcc588c3c838c0a5871b96b8
Dewald.Mouton <moutondewald@gmail.com>
2017-08-10 13:40:19 +0200

    Major Update
    Finilizing most imports and social media displays.

---
+++ Younglings101 Website/js/instagramloader.js
@@ -0,0 +1,6 @@
+var feed = new Instafeed({
+      get: 'user',
+      userId: '5749859234', // Ex: 1374300081
+      accessToken: '5749859234.1677ed0.5e754778d2be4087a80434a5060f4275'
+    });
+    feed.run();
