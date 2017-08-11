627b59e76d069266e032b307336ff5495dfd109d
E-Kaese <ernstkaese@gmail.com>
2017-08-11 09:55:13 +0200

    Fri Aug 11 09:55:08 CAT 2017

--- Younglings101 Website/js/currentYounglingRouting.js
+++ Younglings101 Website/js/currentYounglingRouting.js
@@ -11,23 +11,10 @@ app.config(function ($routeProvider) {
         template: ""
     });
 })
-// .directive('card', function() {
-//     return {
-//         restrict: 'E',
-//         template: "<li class='col-xs-4'><figure class='younglings'><img src='{{}}' alt='a picture' class='img-responsive'/><figcaption><h3>{{name}} <span>{{surname}}</span></h3></figcaption></figure></li>"
-//     };
-// })
-.controller('randomiser', function(){
-    var links = [{"name":"Ernst", "surname":"Kaese", "link":"#!ernst","src":"../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, {"name":"Malcolm", "surname":"TBD", "link":"#!malcolm","src":"../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}];
-    /*
-    The carousel has 5 slides containing 3 cards each.
-    Each card needs to display random objects from links array.
-    Links array can also be a JSON file.
-    Use $scope to add data to the card directive.
-    */
-    links = shuffle(links);
-    console.table(links);
-})
+.controller('randomiser', ['$scope', function($scope) {
+    $scope.links = [{"name":"Ernst", "surname":"Kaese", "link":"#!ernst","src":"../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, {"name":"Malcolm", "surname":"Carelse", "link":"#!malcolm","src":"../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}];
+    links = shuffle($scope.links);
+}])
 .directive('carousel', function(){
         return{
             restrict: "E",
