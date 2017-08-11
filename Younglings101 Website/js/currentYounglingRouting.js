1371a8183a2ac46e7b6906751aaa072f01cb5db0
E-Kaese <ernstkaese@gmail.com>
2017-08-11 09:12:28 +0200

    Fri Aug 11 09:12:24 CAT 2017

--- Younglings101 Website/js/currentYounglingRouting.js
+++ Younglings101 Website/js/currentYounglingRouting.js
@@ -11,31 +11,45 @@ app.config(function ($routeProvider) {
         template: ""
     });
 })
-.directive('card', function() {
-    return {
-        restrict: 'E',
-        template: "<li class='col-xs-4'><figure class='younglings'><img src='http://via.placeholder.com/350x500' alt='a picture' class='img-responsive'/><figcaption><h3>{{name}} <span>{{surname}}</span></h3></figcaption></figure></li>"
-    };
-})
+// .directive('card', function() {
+//     return {
+//         restrict: 'E',
+//         template: "<li class='col-xs-4'><figure class='younglings'><img src='{{}}' alt='a picture' class='img-responsive'/><figcaption><h3>{{name}} <span>{{surname}}</span></h3></figcaption></figure></li>"
+//     };
+// })
 .controller('randomiser', function(){
-    var links = [{"name":"Ernst", "surname":"Kaese", "link":"#!ernst","src":"../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, {"name":"Malcolm", "surname":"TBD", "link":"#!malcolm","src":"../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}];
+    var links = [{"name":"Ernst", "surname":"Kaese", "link":"#!ernst","src":"../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, {"name":"Malcolm", "surname":"TBD", "link":"#!malcolm","src":"../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}, {"name":"", "surname":"", "link":"","src":""}];
     /*
     The carousel has 5 slides containing 3 cards each.
     Each card needs to display random objects from links array.
     Links array can also be a JSON file.
     Use $scope to add data to the card directive.
     */
-    var random = [];
-    var nums = [];
-    while(random.length<links.length){
-        var ran = Math.floor(Math.random*links.length+1);
-        console.log(ran);
-        var flag = false;
-        for(var i=0; i<nums.length; i++){
-            if(nums[i]==ran){
-                
-            }
-        }
-    }
+    links = shuffle(links);
+    console.table(links);
+})
+.directive('carousel', function(){
+        return{
+            restrict: "E",
+            templateUrl: "../pages/younglingsProfilesSites/carousel.html"
+        };
 });
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
