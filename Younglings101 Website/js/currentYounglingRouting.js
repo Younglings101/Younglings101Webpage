d5c7979729065ccb9f9e2639a4e048bc91026ad3
E-Kaese <ernstkaese@gmail.com>
2017-08-15 15:07:00 +0200

    Tue Aug 15 15:06:54 CAT 2017

--- Younglings101 Website/js/currentYounglingRouting.js
+++ Younglings101 Website/js/currentYounglingRouting.js
@@ -1,42 +1,38 @@
-var app = angular.module("main", ["ngRoute"]);
-app.config(function ($routeProvider) {
-    $routeProvider
-    .when("/ernst", {
-        templateUrl: "../pages/younglingsProfilesSites/ernst/index.html"
-    })
-    .when("/malcolm", {
-        templateUrl: "../pages/younglingsProfilesSites/malcolm/malcolmyoungling.html"
-    })
-    .otherwise({
-        template: ""
-    });
-})
-.controller('randomiser', ['$scope', function($scope) {
-    $scope.links = [{"name":"Ernst", "surname":"Kaese", "link":"#!ernst","src":"../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, {"name":"Malcolm", "surname":"Carelse", "link":"#!malcolm","src":"../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}, {"name":"", "surname":"", "link":"","src":"http://via.placeholder.com/350x500"}];
-    links = shuffle($scope.links);
-}])
-.directive('carousel', function(){
-        return{
-            restrict: "E",
-            templateUrl: "../pages/younglingsProfilesSites/carousel.html"
-        };
-});
+var app = angular.module("main", [])
+    .controller('randomiser', ['$scope', '$sce', function ($scope, $sce) {
+        $scope.links = [{ "name": "Ernst", "surname": "Kaese", "link": "../pages/younglingsProfilesSites/ernst/index.html", "src": "../pages/younglingsProfilesSites/ernst/img/faceshot.jpg" }, { "name": "Malcolm", "surname": "Carelse", "link": "../pages/younglingsProfilesSites/malcolm/malcolmyoungling.html", "src": "../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/350x350" }];
+        links = shuffle($scope.links);
+
+        $scope.currentUrl = "";
+        $scope.setCurrentUrl = function (url) {
+            $scope.currentUrl = url;
+        }
+
+        $scope.getUrl = function () {
+            console.log($scope.currentUrl);
+            return $sce.trustAsResourceUrl($scope.currentUrl);
+        }
+    }]);
 
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-  return array;
+    return array;
+}
+
+function resizeIframe(iframe) {
+    iframe.height = iframe.contentWindow.document.body.scrollHeight + "px";
 }
