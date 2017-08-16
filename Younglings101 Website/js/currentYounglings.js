0bd5b26bde0639fb0b5b0a2ccda3549805916f96
E-Kaese <ernstkaese@gmail.com>
2017-08-16 10:39:32 +0200

    Wed Aug 16 10:39:29 CAT 2017

--- Younglings101 Website/js/currentYounglings.js
+++ Younglings101 Website/js/currentYounglings.js
@@ -1,6 +1,6 @@
 var app = angular.module("main", [])
     .controller('randomiser', ['$scope', '$sce', function ($scope, $sce) {
-        $scope.links = [{ "name": "Ernst", "surname": "Kaese", "link": "../pages/younglingsProfilesSites/ernst/index.html", "src": "../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, { "name": "Malcolm", "surname": "Carelse", "link": "../pages/younglingsProfilesSites/malcolm/malcolmyoungling.html", "src": "../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}, { "name": "Athandile", "surname": "Tembile", "link": "../pages/younglingsProfilesSites/athandile/index.html", "src": "../pages/younglingsProfilesSites/athandile/Images/Head.jpg"}, { "name": "Keenan", "surname": "Idas", "link": "../pages/younglingsProfilesSites/keenan/index.html", "src": "../pages/younglingsProfilesSites/keenan/images/Keenan_Idas.jpg" }, { "name": "Zahirah", "surname": "Ismail", "link": "../pages/younglingsProfilesSites/zahirah/index.html", "src": "../pages/younglingsProfilesSites/zahirah/img/z3.jpg" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }];
+        $scope.links = [{ "name": "Ernst", "surname": "Kaese", "link": "../pages/younglingsProfilesSites/ernst/index.html", "src": "../pages/younglingsProfilesSites/ernst/img/faceshot.jpg"}, { "name": "Malcolm", "surname": "Carelse", "link": "../pages/younglingsProfilesSites/malcolm/malcolmyoungling.html", "src": "../pages/younglingsProfilesSites/malcolm/IMG_20170624_130227.jpg"}, { "name": "Athandile", "surname": "Tembile", "link": "../pages/younglingsProfilesSites/athandile/index.html", "src": "../pages/younglingsProfilesSites/athandile/Images/Head.jpg"}, { "name": "Keenan", "surname": "Idas", "link": "../pages/younglingsProfilesSites/keenan/index.html", "src": "../pages/younglingsProfilesSites/keenan/images/Keenan_Idas.jpg" }, { "name": "Zahirah", "surname": "Ismail", "link": "../pages/younglingsProfilesSites/zahirah/index.html", "src": "../pages/younglingsProfilesSites/zahirah/img/z3.jpg" }, { "name": "Zafiera", "surname": "Davids", "link": "../pages/younglingsProfilesSites/zafiera/index.html", "src": "../pages/younglingsProfilesSites/zafiera/img/logo.png" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }, { "name": "", "surname": "", "link": "", "src": "http://via.placeholder.com/400x400" }];
         links = shuffle($scope.links);
 
         $scope.currentUrl = "";
@@ -20,6 +20,12 @@ var app = angular.module("main", [])
             else {
                 frame.style.height = window.innerHeight;
             }
+        }
+
+        $scope.iframeMinimise = function(){
+            // var frame = document.getElementById("frame");
+            // frame.style.height = '0%';
+            $scope.setCurrentUrl("");
         }
     }]);
 
